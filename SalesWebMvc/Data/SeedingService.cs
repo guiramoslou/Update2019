0592c31a00e10b6010545e6702aac6297c2f2515
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2020-09-08 16:08:53 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SalesWebMvc.Models;
+﻿using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
+using System;
+using System.Linq;
 
 namespace SalesWebMvc.Data
 {
